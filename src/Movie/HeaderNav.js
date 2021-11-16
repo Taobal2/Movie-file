091804e7bbab21709be80dc1517b37036441de40
Balogun Taoheed <baloguntaoheed2@gmail.com>
2021-11-16 19:02:10 +0100

    added to the detailed page

--- src/Movie/HeaderNav.js
+++ src/Movie/HeaderNav.js
@@ -1,89 +1,85 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
-import {AiOutlineSetting, AiOutlineHome, AiOutlineFlag, AiFillGithub} from "react-icons/ai";
-import {GrGamepad} from "react-icons/gr";
-import {ImProfile} from "react-icons/im";
-import {CgProfile} from "react-icons/cg";
-import {MdTimer} from "react-icons/md";
-import {GiFoodTruck, GiUnstableProjectile} from "react-icons/gi"
+import {
+  AiOutlineSetting,
+  AiOutlineHome,
+  AiOutlineFlag,
+  AiFillGithub,
+} from "react-icons/ai";
+import { GrGamepad } from "react-icons/gr";
+import { ImProfile } from "react-icons/im";
+import { CgProfile } from "react-icons/cg";
+import { MdTimer } from "react-icons/md";
+import { GiFoodTruck, GiUnstableProjectile } from "react-icons/gi";
 import image from "../Movie/Warner.jpg";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export const HeaderNav = () => {
-    return (
-        <Container>
-            <Wrapper>
-                <Logo src={image}/>
-                <MyLinks>
-                    
-                    <Links to="/"><AiOutlineHome/></Links>
-                    <Links to="/details"><GiFoodTruck/></Links>
-                    <Links  ><AiOutlineFlag/></Links>
-                    {/* <Links><AiFillGithub/></Links>
-                    <Links ><GiUnstableProjectile/></Links>
-                    <Links ><MdTimer/></Links>
-                    <Links ><ImProfile/></Links>
-                    <Links ><GrGamepad/></Links>
-                     */}
-                    
-                </MyLinks>
-            </Wrapper>
-        </Container>
-    )
-}
-
-
-
+  return (
+    <Container>
+      <Wrapper>
+        <Logo src={image} />
+        <MyLinks>
+          <Links to="/">
+            <AiOutlineHome />
+          </Links>
+          <Links>
+            <GiFoodTruck />
+          </Links>
+          <Links>
+            <AiOutlineFlag />
+          </Links>
+        </MyLinks>
+      </Wrapper>
+    </Container>
+  );
+};
 
 const Container = styled.div`
-width:100%;
-height:80px;
-background-color:#000000;
-position:fixed;
-z-index:100;
+  width: 100%;
+  height: 80px;
+  background-color: #000000;
+  position: fixed;
+  z-index: 100;
 `;
 const Wrapper = styled.div`
-width:100%;
-height:100%;
-display:flex;
-justify-content:space-between;
-align-items:center
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const Logo = styled.img`
-width:150px;
-height:70px;
-object-fit:cover;
-margin-left:20px;
+  width: 150px;
+  height: 70px;
+  object-fit: cover;
+  margin-left: 20px;
 `;
 
 const MyLinks = styled.div`
-display:flex;
-margin-right:20px;
-
-
-
+  display: flex;
+  margin-right: 20px;
 `;
 
 const Links = styled(Link)`
-margin: 0 10px;
-text-decoration:none;
-width:60px;
-height:40px;
-border-radius:4px;
-background-color:white;
-display:flex;
-justify-content:center;
-align-items:center;
-color:${({clr}) => (clr ? "yellow" : "black")};
-transition:all 400ms;
-transform:scale(1);
+  margin: 0 10px;
+  text-decoration: none;
+  width: 60px;
+  height: 40px;
+  border-radius: 4px;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${({ clr }) => (clr ? "yellow" : "black")};
+  transition: all 400ms;
+  transform: scale(1);
 
-:hover{
-    background-color:#f2f2f2;
-    transform:scale(1.05);
-    cursor:pointer;
-    color:red;
-
-}
+  :hover {
+    background-color: #f2f2f2;
+    transform: scale(1.05);
+    cursor: pointer;
+    color: red;
+  }
 `;
--- src/Movie/MovieDetails.js
+++ src/Movie/MovieDetails.js
@@ -1,96 +1,85 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import {useParams} from "react-router-dom"
-import data from "./data.json"
-
+import { useParams } from "react-router-dom";
+import data from "./data.json";
 
 export const MovieDetails = () => {
+  const { id } = useParams();
 
-    const {id} = useParams()
+  const [myData, setMyData] = useState([]);
 
-    const [myData, setMyData]=useState([]);
+  useEffect(() => {
+    setMyData(data[id - 1]);
+    console.log(myData);
+  }, []);
 
-    useEffect(()=>{
-        setMyData(data[id-1]);
-        console.log(myData)
-
-    },[])
-
-    return (
-        <Container>
-            <Background src={myData.backgroundImg}/>
-            <Shadow/>
-            <Wrapper>
-                <Title>{myData?.title}</Title>
-                <Image src={myData.titleImg} />
-                <Sub>{myData.subTitle}</Sub>
-                <Desc>{myData.description}</Desc>
-            </Wrapper>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <Background src={myData.backgroundImg} />
+      <Shadow />
+      <Wrapper>
+        <Title>{myData?.title}</Title>
+        <Image src={myData.titleImg} />
+        <Sub>{myData.subTitle}</Sub>
+        <Desc>{myData.description}</Desc>
+      </Wrapper>
+    </Container>
+  );
+};
 const Image = styled.img`
-width:500px;
-height:300px;
-object-fit:contain;
-
+  width: 500px;
+  height: 300px;
+  object-fit: contain;
 `;
 
 const Title = styled.div`
-font-size:30px;
-text-transform:uppercase;
-margin-bottom:30px;
+  font-size: 30px;
+  text-transform: uppercase;
+  margin-bottom: 32px;
 `;
 
 const Sub = styled.div`
-font-size:20px;
-margin:20px 0;
+  font-size: 20px;
+  margin: 20px 0;
 `;
 
 const Desc = styled.div`
-font-size:23px;
+  font-size: 23px;
 `;
 
 const Shadow = styled.img`
-width:100%;
-height:100%;
-border-radius:10px;
-background-color:rgba(0,0,0,0.5);
-position:absolute;
-left:0;
-transition:all 400ms;
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  background-color: rgba(0, 0, 0, 0.5);
+  position: absolute;
+  left: 0;
+  transition: all 400ms;
 
-
-:hover{
-background-color:rgba(0,0,0,0);
-
-}
-
+  :hover {
+    background-color: rgba(0, 0, 0, 0);
+  }
 `;
 
-const Container=styled.div`
-padding-top:79px;
-width:100%;
-height:100%;
-min-height:100vh;
+const Container = styled.div`
+  padding-top: 79px;
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
 `;
-const Wrapper=styled.div`
-position:absolute;
-top:0px;
-left:50px;
-padding-top:200px;
-color:white;
-display:flex;
-flex-direction:column;
-
-
-
-
+const Wrapper = styled.div`
+  position: absolute;
+  top: 0px;
+  left: 50px;
+  padding-top: 201px;
+  color: white;
+  display: flex;
+  flex-direction: column;
 `;
-const Background=styled.img`
-width:100%;
-height:100vh;
-background-color:red;
-object-fit:cover;
-position:relative;
+const Background = styled.img`
+  width: 100%;
+  height: 100vh;
+  background-color: red;
+  object-fit: cover;
+  position: relative;
 `;
